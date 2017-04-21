b0dbb2ca5345a4dfd0a1b100bec1c007cd4ecc05
oswilehi <oswilehi@gmail.com>
2017-04-21 10:13:16 -0600

    Added PaintKeyword() and I got a doubt about the dictionary
    Where do we have to set the buttons, Form1, Form2, both?
    Buttons that I think we need:
    - Cargar Archivo
    - Usar Diccionario Predeterminado

--- Proyecto/Proyecto01/Proyecto01/Form1.Designer.cs
+++ Proyecto/Proyecto01/Proyecto01/Form1.Designer.cs
@@ -48,6 +48,7 @@
             this.richTextBox1.Size = new System.Drawing.Size(621, 222);
             this.richTextBox1.TabIndex = 1;
             this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
             // 
             // Form1
             // 
